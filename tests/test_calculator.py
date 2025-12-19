71980c8c2189ef22d2a27f9aa131885c600b2d47
Marijan Miletić <marijan.miletic@rittmeyer.com>
2025-12-19 17:32:08 +0100

    Feat: Implementiran kalkulator i testovi

---
+++ tests/test_calculator.py
@@ -0,0 +1,16 @@
+﻿import unittest
+from src.calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
